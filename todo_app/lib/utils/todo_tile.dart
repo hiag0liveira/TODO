d76d86b00a1b127414536d5f2b9b5eab3822ef29
LucasPetruci <lucaspetruci100@gmail.com>
2024-06-14 14:14:27 -0300

    fix: move child to end of argument

--- todo_app/lib/utils/todo_tile.dart
+++ todo_app/lib/utils/todo_tile.dart
@@ -17,6 +17,12 @@ class ToDoTile extends StatelessWidget {
       padding: const EdgeInsets.only(left: 25.0, right: 25.0, top: 25.0),
       child: Container(
         padding: EdgeInsets.all(12),
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          border: Border(
+            bottom: BorderSide(color: Colors.grey, width: 0.5),
+          ),
+        ),
         child: Row(
           children: [
             //checkbox
@@ -28,12 +34,6 @@ class ToDoTile extends StatelessWidget {
             //task name
             Text(taskName),
           ],
-        ),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          border: Border(
-            bottom: BorderSide(color: Colors.grey, width: 0.5),
-          ),
         ),
       ),
     );
